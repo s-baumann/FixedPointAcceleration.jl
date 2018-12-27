5726b9cb5d4bce22e421339830f8e4413ee98fbe
Stuart Baumann <stuart@stuartbaumann.com>
2018-12-27 17:58:00 +1100

    Revert "Testing for Scalar functions."
    This reverts commit ffe283c2680ef05a42a1da1fbb212263e321a842.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,5 @@ using Test
 # Run tests
 println("Test putting together iterates with jumps")
 @time @test include("TestPuttingInputsAndOutputsTogether.jl")
-println("Test simple Scalar function")
-@time @test include("SimpleScalarFunction.jl")
 println("Test simple vector function")
 @time @test include("SimpleVectorFunction.jl")
