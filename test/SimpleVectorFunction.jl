954996df335fa813f34d995895016e47a89d1278
s-baumann <stuart@stuartbaumann.com>
2020-12-10 22:33:08 +0000

    Better testing of errors.

--- test/SimpleVectorFunction.jl
+++ test/SimpleVectorFunction.jl
@@ -32,8 +32,16 @@ fp_simple2.Convergence_ < 1e-10
 
 # Testing function that returns a missing
 f(x) = [missing, missing]
-fp_simple3   = fixed_point(f, Inputs; Algorithm = :Simple, PrintReports = true)
+fp_simple3   = fixed_point(f, inputs; Algorithm = :Simple, PrintReports = true)
 fp_simple3.TerminationCondition_ == :InvalidInputOrOutputOfIteration
 f(x) = [1, missing]
 fp_simple4   = fixed_point(f, Inputs; Algorithm = :Simple, PrintReports = true)
 fp_simple4.TerminationCondition_ == :InvalidInputOrOutputOfIteration
+
+# Testing the outputting of a tuple
+f(x) = (a = [1.0, 1.0], b = :goodProgressInFunction)
+@test_throws ErrorException("The Fixedpoint function can only return a vector or a tuple  of which the first entry is the vector for which a fixedpoint is sought and the second is a namedtuple (the contents of which are output for the user but are not used in fixed point acceleration).") fixed_point(f, Inputs; Algorithm = :Simple, PrintReports = true)
+# And doing side effects properly.
+f(x) = ([1.0, 1.0], (b = :goodProgressInFunction,))
+fp_simple5 = fixed_point(f, Inputs; Algorithm = :Simple, PrintReports = true)
+fp_simple5.Convergence_ < 1e-10
