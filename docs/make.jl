2288fc3781e873b674db7650ed39bef9b0792769
s-baumann <stuart@stuartbaumann.com>
2021-09-20 23:33:05 +0100

    Improving Documentation

--- docs/make.jl
+++ docs/make.jl
@@ -4,13 +4,14 @@ makedocs(
     format = Documenter.HTML(),
     sitename = "FixedPointAcceleration",
     modules = [FixedPointAcceleration],
-    pages = ["index.md",
-             "1_FixedPoints.md",
-             "2_Algorithms.md",
-             "3_UsingAdvice.md",
-             "4_Applications.md",
-             "5_TerminationConditions.md",
-             "99_refs.md"]
+    pages = ["Index" => "index.md",
+             "Introduction" => "1_FixedPoints.md",
+             "Algorithms" => "2_Algorithms.md",
+             "Advice in using package" => "3_UsingAdvice.md",
+             "Applications" => "4_Applications.md",
+             "Termination Conditions" => "5_TerminationConditions.md",
+             "API" => "api.md"
+             "References" => "99_refs.md"]
 )
 
 deploydocs(
---
+++ docs/src/6_api.md
@@ -0,0 +1,30 @@
+```@meta
+CurrentModule = FixedPointAcceleration
+```
+
+# Function API
+
+```@index
+Pages = ["api.md"]
+```
+
+### Main fixed point seeking function
+
+```@docs
+    fixed_point
+    fixed_point_new_input
+```
+
+### Structs
+
+```@docs
+    FunctionEvaluationResult
+    FixedPointResults
+```
+
+### Internal Functions
+
+```@docs
+    put_together_without_jumps
+    execute_function_safely
+```
