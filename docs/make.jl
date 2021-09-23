21a5b80ee2fe08759df2a2158fac6d294dc1ac39
Stuart Baumann <stuart@stuartbaumann.com>
2021-09-23 13:09:25 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(
     pages = Any["Index" => "index.md",
              "Introduction" => "1_FixedPoints.md",
              "Algorithms" => "2_Algorithms.md",
-             "Advice in using package" => "3_UsingAdvice.md",
+             "Using package" => "3_UsingAdvice.md",
              "Applications" => "4_Applications.md",
              "Termination Conditions" => "5_TerminationConditions.md",
              "API" => "6_api.md",
