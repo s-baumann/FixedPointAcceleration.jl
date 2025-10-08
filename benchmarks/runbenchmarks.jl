6128a6458b4d28e286a9e4c512fa289b4e8705f2
Orjan Ameye <orjan.ameye@hotmail.com>
2025-10-08 19:35:22 +0200

    build(ci): add modern CI

--- benchmarks/runbenchmarks.jl
+++ benchmarks/runbenchmarks.jl
@@ -1,4 +1,12 @@
 using BenchmarkTools
 using FixedPointAccelerationNext
 
-include("legacy_vs_core.jl")
+# include("legacy_vs_core.jl") # compare legacy vs new implementation
+
+const SUITE = BenchmarkGroup()
+
+BenchmarkTools.tune!(SUITE)
+results = BenchmarkTools.run(SUITE; verbose=true)
+display(median(results))
+
+BenchmarkTools.save("benchmarks_output.json", median(results))
